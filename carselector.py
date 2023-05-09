d1894d497bbe14db29406cf53c279157f21e4ad1
Angelo Davis <francisdavia11@gmail.com>
2023-05-09 15:18:48 +0100

    Basic menu functionality implemented
    We have working buttons, a cycler, text being displayed. Code is a bit spaghetti with a mix of global and passed variables.

--- carselector.py
+++ carselector.py
@@ -1,5 +1,63 @@
 import pygame as pg
 import header as hd
+def test():
+    print("test_success")
+def next(pos, sequence):
+    "if pos"
+    
+
+def last(sequence):
+    "a"
+
+
+class Button():
+    def __init__(self, x, y, width, height, buttonText="Button", onclickFunction=None, onePress=False, buttonImage = None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+        self.alreadyPressed = False
+
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+
+        self.buttonSurface = pg.Surface((self.width, self.height))
+        self.buttonRect = pg.Rect(self.x, self.y, self.width, self.height)
+        if buttonImage != None: 
+            self.buttonSurf = pg.transform.scale_by(pg.image.load(hd.os.path.join('assets', buttonImage)).convert_alpha(), 10)
+        elif buttonText != None:
+            self.buttonSurf = hd.Text_Font.render(buttonText, True, (20, 20, 20))  
+ 
+        hd.buttons.append(self)
+
+    def process(self, screen):
+        """
+        Tracks mouse location and if mouse intersects with the button there is a colour change.
+        Upon click 
+        """
+        mousePos = pg.mouse.get_pos()
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+            if pg.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+                if self.onePress:
+                    self.onclickFunction()
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+            else:
+                self.alreadyPressed = False
+        self.buttonSurface.blit(self.buttonSurf, [
+        self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+        self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+            ])
+        screen.blit(self.buttonSurface, self.buttonRect)
 
 
 
@@ -13,16 +71,27 @@ def fill(surface, color):
             surface.set_at((x, y), pg.Color(r, g, b, a))
 
 class vehicle:
-    def __init__(self, colour_image, detail_image, health = 10, speed = 10, handling = 10, price = 100, owned = True):
-        self.colour_surface = pg.transform.scale_by(pg.image.load(colour_image).convert_alpha(), 10)
-        self.detail_surface = pg.transform.scale_by(pg.image.load(detail_image).convert_alpha(), 10)
-        self.name = detail
+    def __init__(self, 
+                 colour_image, 
+                 detail_image, 
+                 screen,
+                 position = (0,0),
+                 health = 10, 
+                 speed = 10, 
+                 handling = 10, 
+                 price = 100, 
+                 owned = True):
+        self.scale = screen.get_size()[1]/96
+        self.colour_surface = pg.transform.scale_by(pg.image.load(hd.os.path.join('assets', colour_image)).convert_alpha(), self.scale)
+        self.detail_surface = pg.transform.scale_by(pg.image.load(hd.os.path.join('assets', detail_image)).convert_alpha(), self.scale)
+        self.size = self.colour_surface.get_size()
+        self.name = detail_image[:-4]
         self.health = health
         self.speed = speed
         self.handling = handling
         self.price = price
         self.owned = owned
-    
+        self.position = position
     def colour(self, colour):
         """
         Input: colour name string in white black red yellow green blue
@@ -30,73 +99,88 @@ class vehicle:
         """
         fill(self.colour_surface, pg.Color(colour))
 
+    def handle_movement(self):
+        "up"
+        "down"
 
-def draw(vehicle, mode):
-    "a"
+    def draw(self, screen):
+        "a"
 
+
+class upgrade:
+    def __init__(self, image, scale, owned = True, equipped = False):
+        self.surface = pg.transform.scale_by(pg.image.load(image).convert_alpha(), scale)
+        self.name = image[:-4]
+        self.owned = owned
+        self.equipped = equipped
+
+
+
+def draw_selector(screen, vehicle):
+    sx, sy = screen.get_size()
+    bx, by = hd.Background.get_size()
+    vx, vy = vehicle.size
+    Background = pg.transform.scale(hd.Background, ((sy/by)*bx, sy))
+    screen.blit(Background, (0,0))
+    screen.blit(vehicle.colour_surface, (4*sx/5-vx/2, (sy-vy)/2))
+    screen.blit(vehicle.detail_surface, (4*sx/5-vx/2, (sy-vy)/2))
+    nametag = hd.Title_Font.render(vehicle.name, True, (200, 200, 200))
+    health = hd.Title_Font.render(str(vehicle.health) + " HP", True, (200, 200, 200))
+    screen.blit(nametag, (sx/20, sy-sy/2-nametag.get_size()[1]/2))
+    screen.blit(health, (sx-sx/20-health.get_size()[0], sy/8-health.get_size()[1]/2))
+    for i in hd.buttons:
+        i.process(screen)
+
+def colour_test(event, bean):
+    if event.type == pg.KEYDOWN:
+        if event.key == pg.K_f:
+            
+            bean.colour(hd.colours["white"])
+        if event.key == pg.K_g:
+            
+            bean.colour(hd.colours["black"])
+        if event.key == pg.K_h:
+            
+            bean.colour(hd.colours["red"])
     
 
-
-
-
- 
-
-
-
+    
 def main():
     run = True
     clock = pg.time.Clock()
-    screen = DISPLAYSURF = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+
     clock = pg.time.Clock()
-    bean = vehicle("assets/panthercolour.png", "assets/panther.png")
+    hd.vehicles = hd.cycler([vehicle("beancolour.png", "bean.png", hd.screen, health = 5),
+    vehicle("jeepcolour.png", "jeep.png", hd.screen, health = 10),
+    vehicle("bugatticolour.png", "bugatti.png", hd.screen, health = 10),
+    vehicle("panthercolour.png", "panther.png", hd.screen, health = 50)])
+    sx, sy = hd.screen.get_size()
+    button_size = (100, 100)
+    button = Button(sx-sx/5-button_size[0]/2,3*sy/4-button_size[1]/2, 100, 100, buttonImage = "down.png", onclickFunction = hd.vehicles.forward)
+    button = Button(sx-sx/5-button_size[0]/2 ,sy/4-button_size[1]/2, 100, 100, buttonImage = "up.png", onclickFunction = hd.vehicles.reverse)
+    button = Button(sx/2-3*button_size[0]/2,sy/4-button_size[1]/2, 100, 100, buttonImage = "left.png", onclickFunction = hd.colours_cycler.forward)
+    button = Button(sx/2+1.5*button_size[0]/2 ,sy/4-button_size[1]/2, 100, 100, buttonImage = "right.png", onclickFunction = hd.colours_cycler.reverse)
+    # Uncomment this for a non-translucent surface.
     # Uncomment this for a non-translucent surface.
     # surface = pg.Surface((100, 150), pg.SRCALPHA)
-    # pg.draw.circle(surface, pg.Color(40, 240, 120), (50, 50), 50)
-    surface = pg.image.load('assets/bugatticolour.png').convert_alpha()
-    overlay = pg.image.load("assets/bugatti.png").convert_alpha()
-    surface = pg.transform.rotozoom(surface, 180, 1)
-    surface = pg.transform.scale_by(surface, 10)
-
-    done = False
+    # pg.draw.circle(surface, pg.Color(40, 240, 120), (50, 50), 50)  
     counter = 1
     while run:
         clock.tick(hd.FPS)
-        print(counter)
-        counter += 1
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
                 pg.quit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_f:
-                    fill(surface, pg.Color(hd.colours["white"]))
-                    bean.colour(hd.colours["white"])
-                if event.key == pg.K_g:
-                    fill(surface, pg.Color(hd.colours["black"]))
-                    bean.colour(hd.colours["black"])
-                if event.key == pg.K_h:
-                    fill(surface, pg.Color(hd.colours["red"]))
-                    bean.colour(hd.colours["red"])
+        hd.vehicles.item.colour(hd.colours_cycler.item)
+        draw_selector(hd.screen, hd.vehicles.item)
 
-        screen.fill(pg.Color('lightskyblue4'))
-        pg.draw.rect(screen, (40, 50, 50), (0, 0, 50, 50))
-        surface.blit(overlay, (100,100))
-        screen.blit(surface, (100, 200))
-       
-        pg.draw.rect(surface, pg.Color(40, 50, 50), (0, 0, 50, 50))
-        #bean.colour_surface.blit(bean.detail_surface, (0,0))
-        screen.blit(bean.colour_surface, (0,0))
-        screen.blit(bean.detail_surface, (0,0))
-        #bean.colour_surface.blit(bean.detail_surface, (0,0))
         pg.display.update()
+        
 
 
 
 if __name__ == '__main__':
-    
-
     main()
     pg.quit()
  
-pygame.quit()
-quit()
+
--- header.py
+++ header.py
@@ -1,13 +1,47 @@
+#made this to store global variables so I don't get global conflics
 import pygame as pg
-
+import os
+pg.font.init()
+screen = DISPLAYSURF = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+screen = pg.display.set_mode((1000, 800))
+resolution = screen.get_size()
+Title_Font = pg.font.SysFont('Squarefont', 100)
+Text_Font = pg.font.SysFont('Squarefont', 40)
+Background = pg.image.load(os.path.join('assets', 'road.png'))
 colours = {
     "white" : (206,206,206),
-    "black" : (10,10,10),
+    "black" : (30,30,30),
     "red"   : (180, 16, 16),
     "yellow": (237, 186, 41),
     "green" : (46, 228, 91)
     }
 
 FPS = 60
-
 dinero = 0
+buttons = []
+vehicles = None
+class cycler:
+    def __init__(self, items):
+        self.items = items
+        self.index = 0
+        self.item = self.items[0]
+
+    def forward(self):
+        if self.index < (len(self.items)-1):
+            self.index += 1
+            self.item = self.items[self.index]
+        elif self.index >= (len(self.items)-1):
+            self.index = 0
+            self.item = self.items[self.index]
+    def reverse(self):
+        if self.index > 0:
+            self.index -= 1
+            self.item = self.items[self.index]
+        elif self.index <= 0:
+            self.index = (len(self.items)-1)
+            self.item = self.items[self.index]
+
+colours_cycler = cycler([colours[i] for i in colours])
+
+
+        
